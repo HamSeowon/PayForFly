767daa03bb6e9e8499e90da260ad4c99bbc767cf
Seowon Ham <hamseowon2004@gmail.com>
2017-11-26 22:11:30 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -14,10 +14,14 @@ public class Main extends PluginBase implements Listener{
 	@Override
 	public void onEnable(){
 		this.getDataFolder().mkdir();
-		this.config=new Config(getDataFolder()+"config.yml",Config.YAML);
+		this.config=new Config(getDataFolder()+"/config.yml",Config.YAML);
+		if(config.exists("flycost")){
+			this.saveResource("config.yml");
+		}else if(!(config.exists("flycost"))){
+			config.set("flycost",1000);
+		}
 		this.getServer().getPluginManager().registerEvents(this, this);
 		this.getServer().getLogger().info("PayForFly Enabled!-");
-		//config.set("cost",1000d);
 		config.save();
 	}
 	@Override
@@ -32,25 +36,39 @@ public class Main extends PluginBase implements Listener{
 				 sender.sendMessage("플레이어만 사용가능한 명령어입니다.");
 				 return false;
 			 }else{
-				 if(args.length==1){
-					 int min=0;
-					 try {
-						min=Integer.parseInt(args[0]);
-					} catch (NumberFormatException e) {
-						sender.sendMessage("숫자만을 입력해주세요.");
-						return false;
-						}
-					Player player = (Player)sender;
-					double cost = Double.parseDouble(config.get("cost").toString());
-					 if(EconomyAPI.getInstance().myMoney(player)>=cost*min){
+				 int min=0;
+				 try {
+					min=Integer.parseInt(args[0]);
+				} catch (NumberFormatException e) {
+					sender.sendMessage("숫자만을 입력해주세요.");
+					return false;
+					// TODO: handle exception
+					}
+				Player player = (Player)sender;
+				double flycost = Double.parseDouble(config.get("flycost").toString());
+				 if(args.length==0){
+					 if(EconomyAPI.getInstance().myMoney(player)>=flycost){
 							player.setAllowFlight(true);
-							EconomyAPI.getInstance().reduceMoney(player,cost*min);
-							player.sendMessage("플라이가 활성화 되었습니다(-"+cost*min+")");
+							EconomyAPI.getInstance().reduceMoney(player,flycost);
+							player.sendMessage("플라이가 활성화 되었습니다(-"+flycost+")");
 							this.getServer().getScheduler().scheduleDelayedTask(new Task(this,player),20*60*min);
 							return true;
 							
-						}else if(EconomyAPI.getInstance().myMoney(player)<cost*min){
-							player.sendMessage("돈이 부족합니다   필요금액: "+cost*min);
+						}else if(EconomyAPI.getInstance().myMoney(player)<flycost){
+							player.sendMessage("돈이 부족합니다   필요금액: "+flycost);
+							return false;
+						}
+				 }
+				 else if(args.length==1){
+					 if(EconomyAPI.getInstance().myMoney(player)>=flycost*min){
+							player.setAllowFlight(true);
+							EconomyAPI.getInstance().reduceMoney(player,flycost*min);
+							player.sendMessage("플라이가 활성화 되었습니다(-"+flycost*min+")");
+							this.getServer().getScheduler().scheduleDelayedTask(new Task(this,player),20*60*min);
+							return true;
+							
+						}else if(EconomyAPI.getInstance().myMoney(player)<flycost*min){
+							player.sendMessage("돈이 부족합니다   필요금액: "+flycost*min);
 							return false;
 						}
 					}
